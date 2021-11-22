94ba2ffa264c1fbac0ab374312040fe01f99a71b
JakeWh <kolarkuba@gmail.com>
2021-11-22 12:30:09 +0100

    optava kodu atomem

--- palindrom.py
+++ palindrom.py
@@ -1,15 +1,15 @@
 print("Tento prográmek ti řekne, zda tvoje slovo je palindrom.")
 pokracovat = True
 while pokracovat:
-	slovo = input("Zadej slovo: ")
-	slovo_pozpatku = slovo[::-1]
-	if slovo == slovo_pozpatku:
-		print("Tohle slovo je palidrom.")
-	else:
-		print("Tohle slovo není palindrom.")
-	znovu = input("Pokračovat? ano/ne: ")
-	if znovu == "ano":
-		continue
-	else:
-		break
+    slovo = input("Zadej slovo: ")
+    slovo_pozpatku = slovo[::-1]
+    if slovo == slovo_pozpatku:
+        print("Tohle slovo je palidrom.")
+    else:
+        print("Tohle slovo není palindrom.")
+    znovu = input("Pokračovat? ano/ne: ")
+    if znovu == "ano":
+        continue
+    else:
+        break
 input("Stiskem klávesy ukončíš program")
--- zelenina_moje.py
+++ zelenina_moje.py
@@ -3,18 +3,18 @@ ovoce = ["banán", "pomeranč", "citron"]
 pocet_dotazu = 0
 opakovat = True
 while opakovat:
-        nazev = input("Zadej název zeleniny nebo ovoce: ")
-        if nazev in zelenina:
-                print("Napsal jsi zeleninu.")
-        elif nazev in ovoce:
-                print("Napsal jsi ovoce.")
-        else:
-                print("Tohle nemám v seznamu.")
-        pocet_dotazu += 1
-        znovu = input("Chceš zadat další slovo? ano/ne: ")
-        if znovu == "ano":
-                continue
-        else:
-                break
+    nazev = input("Zadej název zeleniny nebo ovoce: ")
+    if nazev in zelenina:
+        print("Napsal jsi zeleninu.")
+    elif nazev in ovoce:
+        print("Napsal jsi ovoce.")
+    else:
+        print("Tohle nemám v seznamu.")
+    pocet_dotazu += 1
+    znovu = input("Chceš zadat další slovo? ano/ne: ")
+    if znovu == "ano":
+        continue
+    else:
+        break
 print("Celkem napsáno dotazů: ", pocet_dotazu)
 input()
