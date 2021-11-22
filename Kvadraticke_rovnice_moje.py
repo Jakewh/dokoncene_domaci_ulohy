05499d1bed46e8144dd76d0aac3f4c0c95dd42ee
JakeWh <kolarkuba@gmail.com>
2021-11-22 12:23:51 +0100

    a

--- Kvadraticke_rovnice_moje.py
+++ Kvadraticke_rovnice_moje.py
@@ -4,11 +4,11 @@ b = int(input("Zadej koeficient b: "))
 c = int(input("Zadej koeficient c: "))
 D = b ** 2 - 4 * a * c
 if D >= 0:
-	x_1 = (- b - D / D) / 2 * a
-	x_2 = (- b + D / D) / 2 * a
-	print("Rovnice má dva kořeny:")
-	print("Kořen jedna je: ", x_1)
-	print("Kořen dvě je: ", x_2)
+    x_1 = (-b - D / D) / 2 * a
+    x_2 = (-b + D / D) / 2 * a
+    print("Rovnice má dva kořeny:")
+    print("Kořen jedna je: ", x_1)
+    print("Kořen dvě je: ", x_2)
 else:
-	print("Rovnice nemá řešení.")
+    print("Rovnice nemá řešení.")
 input()
