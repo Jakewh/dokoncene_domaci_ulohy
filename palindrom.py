94bcbd3538612407b98820b4a69bbf178a9e56ea
JakeWh <kolarkuba@gmail.com>
2021-11-22 10:09:35 +0100

    hotovo
    Dokončeno i s opakováním cyklu

--- palindrom.py
+++ palindrom.py
@@ -1,8 +1,15 @@
 print("Tento prográmek ti řekne, zda tvoje slovo je palindrom.")
-slovo = input("Zadej slovo: ")
-slovo_pozpatku = slovo[::-1]  # neobrací slovo :(
-if slovo == slovo_pozpatku:
-	print("Tohle slovo je palidrom.")
-else:
-	print("Tohle slovo není palindrom.")
-input()
+pokracovat = True
+while pokracovat:
+	slovo = input("Zadej slovo: ")
+	slovo_pozpatku = slovo[::-1]
+	if slovo == slovo_pozpatku:
+		print("Tohle slovo je palidrom.")
+	else:
+		print("Tohle slovo není palindrom.")
+	znovu = input("Pokračovat? ano/ne: ")
+	if znovu == "ano":
+		continue
+	else:
+		break
+input("Stiskem klávesy ukončíš program")
