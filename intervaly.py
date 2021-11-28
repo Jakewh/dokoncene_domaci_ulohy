1a26760d128fa377f3a9ba4b0bae069271e64052
JakeWh <kolarkuba@gmail.com>
2021-11-28 11:05:58 +0100

    novy domaci ukol

---
+++ intervaly.py
@@ -0,0 +1,8 @@
+# první interval
+leva_mez_1 = int(input("Zadejte levou mez 1. intervalu: "))
+prava_mez_1 = int(input("Zadejte pravou mez 1. intervalu: "))
+
+# druhý interval
+leva_mez_2 = int(input("Zadejte levou mez 2. intervalu: "))
+prava_mez_2 = int(input("Zadejte pravou mez 2. intervalu: "))
+
